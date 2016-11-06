6e616394288b59089c4eb19ecb7890298a1e9c27
Liu@iMac <pizza@pizzaliu.com>
2016-11-06 16:10:32 +0800

    feature: store & restore image

--- Homepwner/BNRImageStore.m
+++ Homepwner/BNRImageStore.m
@@ -45,11 +45,25 @@
 - (void)setImage:(UIImage *)image forKey:(NSString *)key
 {
     [self.dictionary setObject:image forKey:key];
+    NSString *imgPath = [self imagePathForKey:key];
+    NSData *imgData = UIImageJPEGRepresentation(image, 0.5);
+    [imgData writeToFile:imgPath atomically:YES];
 }
 
 - (UIImage *)imageForKey:(NSString *)key
 {
-    return [self.dictionary objectForKey:key];
+    UIImage *img = [self.dictionary objectForKey:key];
+    if (!img) {
+        NSString *imgPath = [self imagePathForKey:key];
+        img = [UIImage imageWithContentsOfFile:imgPath];
+        if (!img) {
+            NSLog(@"Unable to find image: %@", key);
+        } else {
+            [self.dictionary setObject:img forKey:key];
+        }
+    }
+
+    return  img;
 }
 
 - (void)deleteImageForKey:(NSString *)key
@@ -58,6 +72,15 @@
         return;
     }
     [self.dictionary removeObjectForKey:key];
+    NSString *imgPath = [self imagePathForKey:key];
+    [[NSFileManager defaultManager] removeItemAtPath:imgPath error:nil];
+}
+
+- (NSString *)imagePathForKey:(NSString *)key
+{
+    NSArray *docPaths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *docPath = [docPaths firstObject];
+    return [docPath stringByAppendingPathComponent:key];
 }
 
 @end
